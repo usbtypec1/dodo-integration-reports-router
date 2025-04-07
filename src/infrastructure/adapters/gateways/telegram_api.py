0bd7e14ff4ddaf89dba44aec9fd0c8ef2dc1a684
Eldos <eldos.baktybekov@gmail.com>
2025-04-07 22:20:06 +0600

    🦺 Enhance error handling in Telegram message sending functionality

--- src/infrastructure/adapters/gateways/telegram_api.py
+++ src/infrastructure/adapters/gateways/telegram_api.py
@@ -1,7 +1,12 @@
+import asyncio
+import logging
 from dataclasses import dataclass
 
 from aiogram import Bot
 from aiogram.enums import ParseMode
+from aiogram.exceptions import TelegramAPIError
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
@@ -9,8 +14,12 @@ class TelegramApiGateway:
     bot: Bot
 
     async def send_message(self, *, chat_id: int, text: str) -> None:
-        await self.bot.send_message(
-            chat_id=chat_id,
-            text=text,
-            parse_mode=ParseMode.HTML,
-        )
+        try:
+            await self.bot.send_message(
+                chat_id=chat_id,
+                text=text,
+                parse_mode=ParseMode.HTML,
+            )
+        except TelegramAPIError:
+            logger.error("Error sending message to chat_id %s", chat_id)
+            await asyncio.sleep(1)

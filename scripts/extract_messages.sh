67a9e913c6ffbd88667419cadfa407d49ef89a0d
Eldos <eldos.baktybekov@gmail.com>
2025-04-07 22:15:06 +0600

    🔨 Update extract_messages.sh to specify source directory and exclude virtual environments

--- scripts/extract_messages.sh
+++ scripts/extract_messages.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 DOMAIN="messages"
-SRC_DIR="."
+SRC_DIR="src"
 
-echo "Extracting translatable strings from $SRC_DIR..."
-xgettext --language=Python --keyword=_ --output="./locales/$DOMAIN.pot" $(find "$SRC_DIR" -name "*.py")
+echo "Extracting translatable strings from $SRC_DIR (excluding venvs)..."
+
+xgettext --language=Python --keyword=_ \
+  --from-code=UTF-8 \
+  --output="./locales/$DOMAIN.pot" \
+  $(find "$SRC_DIR" -name "*.py" -not -path "*/.venv/*" -not -path "*/venv/*")
+
+sed -i '' 's/charset=CHARSET/charset=UTF-8/' ./locales/$DOMAIN.pot
 
 echo "Done extracting strings ✅"

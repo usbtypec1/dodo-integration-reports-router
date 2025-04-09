9cba5218120c5b5f6ce1636b3ad74b83f06e25ab
Eldos <eldos.baktybekov@gmail.com>
2025-04-09 21:05:32 +0600

    ✨ Refactor render_sales_statistics to sort units by sales for today

--- src/presentation/ui/sales_statistics.py
+++ src/presentation/ui/sales_statistics.py
@@ -24,6 +24,13 @@ def render_sales_statistics(
     units_sales_statistics: Iterable[UnitSalesStatistics],
 ) -> list[str]:
     lines: list[str] = [Bold(_("render:sales_statistics:title")).as_html()]
+
+    units_sales_statistics = sorted(
+        units_sales_statistics,
+        key=lambda unit: unit["sales_for_today"],
+        reverse=True,
+    )
+
     for unit_sales_statistics in units_sales_statistics:
         unit_name = unit_sales_statistics["unit_name"]
         sales_for_today = unit_sales_statistics["sales_for_today"]

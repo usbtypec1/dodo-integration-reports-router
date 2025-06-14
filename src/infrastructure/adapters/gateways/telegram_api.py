8d327ddebfd3fd0cea83ff80a05416d9abab0233
Eldos <eldos.baktybekov@gmail.com>
2025-06-15 04:30:03 +0600

    ✨ Enhance TelegramApiGateway: implement retry logic for message sending and improve error handling

--- src/infrastructure/adapters/gateways/telegram_api.py
+++ src/infrastructure/adapters/gateways/telegram_api.py
@@ -4,7 +4,12 @@ from dataclasses import dataclass
 
 from aiogram import Bot
 from aiogram.enums import ParseMode
-from aiogram.exceptions import TelegramAPIError
+from aiogram.exceptions import (
+    TelegramAPIError,
+    TelegramNetworkError,
+    TelegramRetryAfter,
+    TelegramServerError,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -13,14 +18,39 @@ logger = logging.getLogger(__name__)
 class TelegramApiGateway:
     bot: Bot
 
-    async def send_message(self, *, chat_id: int, text: str) -> None:
-        try:
-            await self.bot.send_message(
-                chat_id=chat_id,
-                text=text,
-                parse_mode=ParseMode.HTML,
-            )
-        except TelegramAPIError as error:
-            print(error)
-            logger.error("Error sending message to chat_id %s", chat_id)
-            await asyncio.sleep(1)
+    async def send_message(
+        self,
+        *,
+        chat_id: int,
+        text: str,
+        attempts: int = 10,
+    ) -> None:
+        for attempt in range(attempts):
+            try:
+                await self.bot.send_message(
+                    chat_id=chat_id,
+                    text=text,
+                    parse_mode=ParseMode.HTML,
+                )
+            except (
+                TelegramRetryAfter,
+                TelegramNetworkError,
+                TelegramServerError,
+            ) as error:
+                logger.warning(
+                    "Retrying to send message to chat_id %s after error: %s. Attempt %d/%d",
+                    chat_id,
+                    error,
+                    attempt + 1,
+                    attempts,
+                )
+                await asyncio.sleep(5)
+            except TelegramAPIError as error:
+                logger.error(
+                    "Error sending message to chat_id %s error %s",
+                    chat_id,
+                    error,
+                )
+                break
+            finally:
+                await asyncio.sleep(1)

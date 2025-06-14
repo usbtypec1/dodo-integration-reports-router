310aa1de5b22cb2c583861fc94215374d6662723
Eldos <eldos.baktybekov@gmail.com>
2025-06-15 02:29:14 +0600

    ✨ Enhance docstrings for int_gaps and format_percentage functions to improve clarity and examples

--- src/presentation/ui/common.py
+++ src/presentation/ui/common.py
@@ -1,6 +1,20 @@
 def int_gaps(number: int | float) -> str:
+    """
+    Makes a number look like an integer with gaps.
+
+    For example:
+        - 1234 becomes '1 234',
+        - 1234567 becomes '1 234 567'.
+    """
     return f"{number:_}".replace("_", " ")
 
 
 def format_percentage(number: int | float) -> str:
+    """
+    Makes a number look like a percentage.
+
+    For example:
+        - 12.34 becomes '+12.34%',
+        - 55 becomes '-55%'.
+    """
     return f"{number:+}%"
